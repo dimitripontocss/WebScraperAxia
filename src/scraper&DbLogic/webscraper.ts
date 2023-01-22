7d1d8b03fee0cb4316328fce89d4fb178c16f049
Dimitri <assis.dimitri@engenharia.ufjf.br>
2023-01-22 12:29:25 -0300

    feat: scraper improved

--- src/scraper&DbLogic/webscraper.ts
+++ src/scraper&DbLogic/webscraper.ts
@@ -2,34 +2,58 @@ import * as pup from "puppeteer";
 
 const url = "https://www.reuters.com/markets/currencies";
 
+interface INews {
+  title: string;
+  url: string;
+  date: string | null;
+}
+
 export default async function latestNewsScraper() {
   const browser = await pup.launch();
   const page = await browser.newPage();
   await page.setViewport({ width: 1366, height: 768 });
 
-  await page.goto(url, { timeout: 15000, waitUntil: "domcontentloaded" });
-
-  await page
-    .waitForXPath(
-      "/html/body/div[1]/div/div[3]/div[2]/div/div[1]/div/div[1]/h2/a",
-      { timeout: 10000 }
-    )
-    .catch((e) => console.log(e));
+  await page.goto(url, { waitUntil: "domcontentloaded" });
 
   const latestNews = await page.$eval("#fusion-app", (mainElement) => {
-    const info =
-      mainElement.children[0].children[2].children[1].children[0].children[0]
-        .children[0].children[1].children[0].children[0].children[1];
+    let latestsNews: INews[] = [];
 
-    const url =
-      "https://www.reuters.com" + info.children[1].getAttribute("href");
-    const title = info.children[1].children[0].innerHTML;
-    const date = info.children[2].getAttribute("datetime");
-    return {
-      title,
-      url,
-      date,
-    };
+    for (let cont = 0; cont < 9; cont++) {
+      const news =
+        mainElement.children[0].children[2].children[1].children[0].children[0]
+          .children[0].children[1].children[cont];
+
+      if (news.children[0].getAttribute("data-testid") === "TextStoryCard") {
+        const info = news.children[0];
+
+        const url =
+          "https://www.reuters.com" + info.children[1].getAttribute("href");
+        const title = info.children[1].innerHTML;
+        const date = info.children[3].getAttribute("datetime");
+
+        latestsNews.push({
+          title,
+          url,
+          date,
+        });
+        continue;
+      }
+
+      const info = news.children[0].children[1];
+
+      const url =
+        "https://www.reuters.com" + info.children[1].getAttribute("href");
+      const title = info.children[1].children[0].innerHTML;
+      const date = info.children[2].getAttribute("datetime");
+
+      latestsNews.push({
+        title,
+        url,
+        date,
+      });
+    }
+
+    return latestsNews;
   });
 
   await browser.close();
